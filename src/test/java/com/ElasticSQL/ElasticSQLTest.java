c40b382c1af9f4a58a02bbf2373709eb2ad3b31e
rotem <rotem.hermon@gmail.com>
2012-01-04 00:25:32 +0200

    minor

--- src/test/java/com/ElasticSQL/ElasticSQLTest.java
+++ src/test/java/com/ElasticSQL/ElasticSQLTest.java
@@ -16,7 +16,7 @@ public class ElasticSQLTest {
 	@Test
 	public void test2() {
 		//String sql = "select * from type where id in (\"1\",\"2\",\"3\")";
-		String sql = "select * from type where id in (1, 2, 3)";
+		String sql = "select * from type where id in (1, 2.6, 3)";
 		ElasticSQLParser parser = new ElasticSQLParser();
 		parser.parse(sql);
 	}
